fd14ba5e820004c07f2df996557ddf057134b78a
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-04-15 20:53:56 -0700

    nicer ui

--- calendargpt/src/App.js
+++ calendargpt/src/App.js
@@ -1,36 +1,23 @@
-import React from 'react';
-
 const App = () => {
   return (
-    <div className="App">
-      <section className="sidebar">
-        <button className="new-chat-btn">+ New Chat</button>
-        <ul className="chat-history">
-          <li>Hi there</li>
+    <div className="app">
+      <section className="side-bar">
+        <button>+ New Chat</button>
+        <ul className="history">
+          <li> something </li>
         </ul>
         <nav>
-          <p>Made by Jasan Gill</p>
+          <p>
+            made by jasan
+          </p>
         </nav>
+        
       </section>
       <section className="main">
-        <h1>Calendar Gpt</h1>
-        <ul className="feed">
-          {/* Add some feed items here */}
-        </ul>
-        <div className="bottom-container">
-          <div className='input-container'>
-            <input type="text" placeholder="Type a message" />
-            <div className="send-btn">Send</div>
-            
-          </div>
-          <p className="feature-description">
-            This is a chatbot that can help you schedule your day-to-day activities.
-            </p>
-        </div>
-        
+
       </section>
     </div>
   );
-};
+}
 
 export default App;

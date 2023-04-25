2b998d56c3af373f69e3f3d07b5c11481c5422b6
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-04-24 17:20:05 -0700

    Login Page inital

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import Sidebar from '../components/sidebar'
 import { SessionProvider } from "../components/SessionProvider"
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/pages/api/auth/[...nextauth]'
+import Login from '@/components/Login'
 
 
 export const metadata = {
@@ -22,6 +23,10 @@ export default async function RootLayout({
     <html lang="en">
       <body>
         <SessionProvider session={session}>
+          {!session ? (
+              <Login />
+          ): (
+
             <div className='flex'>
             <div className='bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]'>
               <Sidebar />
@@ -31,6 +36,7 @@ export default async function RootLayout({
 
               <div className='bg-[#342541] flex-1'>{children}</div>
             </div>
+           )}
         </SessionProvider>
       </body>
     </html>

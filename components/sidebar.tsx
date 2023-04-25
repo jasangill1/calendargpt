78a01306ada2b9a480ae5593628589c1e4ec66b4
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-04-24 23:43:13 -0700

    signout function

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,8 +1,11 @@
 // components/sidebar.tsx
+'use client'
 import React from 'react';
 import NewChat from './NewChat';
+import { useSession, signOut } from 'next-auth/react';
 
 const Sidebar = () => {
+  const { data: session } = useSession();
   return (
     <div className='p-2 flex flex-col h-screen'>
       <div className='flex-1'>
@@ -13,6 +16,11 @@ const Sidebar = () => {
            </div>
               {/*Map through the chat rows*/}
       </div>
+      {session && (
+        <img onClick={() => signOut()} src={session.user?.image!} alt="Profile Picture"
+          className='h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50'
+        />
+      )}
     </div>
   );
 };

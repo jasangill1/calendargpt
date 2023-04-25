0bc8e84ec857313185ae04df3e5a53ac9f6d24bf
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-04-24 17:14:35 -0700

    session set up next-auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,37 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
-import Sidebar from 'components/sidebar.tsx'
+import Sidebar from '../components/sidebar'
+import { SessionProvider } from "../components/SessionProvider"
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/pages/api/auth/[...nextauth]'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'CalendarGPT',
   description: 'Takes care of your schdule',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+    const session = await getServerSession(authOptions);
+
   return (
     
     <html lang="en">
-      <body className={inter.className}>
-        <div className='flex'>
-         <div className='bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]'>
-          <Sidebar />
-         </div>
-          
-          {/*ClientProvider*/}
+      <body>
+        <SessionProvider session={session}>
+            <div className='flex'>
+            <div className='bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]'>
+              <Sidebar />
+            </div>
+              
+              {/*ClientProvider*/}
 
-          <div className='bg-[#342541] flex-1'>{children}</div>
-        </div>
+              <div className='bg-[#342541] flex-1'>{children}</div>
+            </div>
+        </SessionProvider>
       </body>
     </html>
   )

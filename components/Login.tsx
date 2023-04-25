a74de7efb1da0b0b227c08ee8d32a7a05a57d08e
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-04-24 17:29:55 -0700

    basic logo and next config

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,6 +1,18 @@
+'use client'
+import {signIn} from "next-auth/react"
+import Image from "next/image";
+
 function Login (){
     return (
-        <div>Login</div>
+        <div>
+            <Image 
+                src= "https://links.papareact.com/t4i"
+                height={400}
+                width={400}
+                alt="logo"
+            />
+            <button>Sign In to use ChatGPT</button>
+        </div>
     )
 
 }
--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['links.papareact.com'],
+  },
   experimental: {
     appDir: true,
   },

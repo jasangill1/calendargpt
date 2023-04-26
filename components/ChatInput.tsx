f004604124932cf7d06066a5793873d28b958fc5
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-04-26 00:21:28 -0700

    chatInput styling

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
+import { useSession } from "next-auth/react";
 import React, { useState } from "react";
 
 
@@ -10,13 +11,21 @@ type Props = {
 
 function ChatInput({chatId}: Props) {
     const [prompt, setPrompt] = useState("");
+    const {data: session} = useSession();
+
     return <div>
         <form className="p-5 space-x-5 flex items-center">
             <input 
-            value={prompt}
-            onChange={e => {setPrompt(e.target.value)}}
-            type="text" placeholder="Type a message" className="w-full px-4 py-2 border border-gray-300 rounded-md bg-white text-black focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent" />
-            <button type="submit" className="ml-2">
+                value={prompt}
+                onChange={e => {setPrompt(e.target.value)}}
+                type="text" 
+                placeholder="Type a message" 
+                className="w-full px-4 py-2 border border-gray-300 rounded-md bg-white text-black focus:outline-none 
+                focus:ring-2 focus:ring-indigo-500 focus:border-transparent" />
+            <button 
+                disabled={!prompt||!session}
+                className="bg-[#11A3FF] text-white px-4 py-2 rounded-md hover:bg-[#0484FF] transition-all]"
+                type="submit" >
                  <PaperAirplaneIcon className="h-4 w-4 -rotate-45"/>   
             </button>  
         </form>

44ef0b1dde3f5ba9c750a242327bf744776ba75d
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-04-26 00:11:14 -0700

    input box and typing

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -11,7 +11,7 @@ type Props = {
 function ChatPage({params: {id}}:Props) {
   return <div className="flex flex-col h-screen overflow-hidden">
     <Chat chatId={id}/>
-    <ChatInput chat={id}/>
+    <ChatInput chatId={id}/>
   </div>;
 }
 
--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,5 +1,7 @@
+'use client'
+
 import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
-import React from "react";
+import React, { useState } from "react";
 
 
 type Props = {
@@ -7,16 +9,19 @@ type Props = {
 }
 
 function ChatInput({chatId}: Props) {
-
+    const [prompt, setPrompt] = useState("");
     return <div>
-        <form className="p-5 space-x-5 flex-1">
-            <input type="text" placeholder="Type a message" className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent" />
-            <button type="submit">
+        <form className="p-5 space-x-5 flex items-center">
+            <input 
+            value={prompt}
+            onChange={e => {setPrompt(e.target.value)}}
+            type="text" placeholder="Type a message" className="w-full px-4 py-2 border border-gray-300 rounded-md bg-white text-black focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent" />
+            <button type="submit" className="ml-2">
                  <PaperAirplaneIcon className="h-4 w-4 -rotate-45"/>   
             </button>  
         </form>
     </div>;
-      
 }
+
 
 export default ChatInput;

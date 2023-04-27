0f3786d57d3f293f74c5dbbf02520fb83bf90066
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-04-27 00:51:49 -0700

    idek

--- app/api/generate-text/route.ts
+++ app/api/generate-text/route.ts
@@ -2,36 +2,28 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import openai from 'openai';
 
-const apiKey = process.env.OPENAI_API_KEY;
-openai.apiKey = apiKey;
+openai.apiKey = process.env.OPENAI_API_KEY;
 
-export async function post(req: NextApiRequest, res: NextApiResponse) {
-  const requestBody = req.body;
-  const prompt = requestBody.prompt;
 
-  try {
-    const result = await openai.Completion.create({
-      engine: 'davinci-codex',
-      prompt: prompt,
-      max_tokens: 100,
-      n: 1,
-      stop: null,
-      temperature: 0.5,
-    });
-
-    const generatedText = result.choices[0].text.trim();
-
-    res.status(200).json({ generatedText });
-  } catch (error) {
-    console.error('Error calling OpenAI API:', error);
-    res.status(500).json({ error: 'Error calling OpenAI API' });
+export async function post(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+    const requestBody = req.body;
+    const prompt = requestBody.prompt;
+  
+    try {
+      const result = await openai.ChatCompletion.create({
+        model: 'gpt-3.5-turbo',
+        messages: [
+          { role: 'system', content: 'You are a helpful assistant.' },
+          { role: 'user', content: prompt },
+        ],
+      });
+  
+      const generatedText = result.choices[0].message.content.trim();
+  
+      res.status(200).json({ generatedText });
+    } catch (error) {
+      console.error('Error calling OpenAI API:', error);
+      res.status(500).json({ error: 'Error calling OpenAI API' });
+    }
   }
-}
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    return post(req, res);
-  }
-
-  res.status(405).json({ error: 'Method not allowed' });
-}
+  

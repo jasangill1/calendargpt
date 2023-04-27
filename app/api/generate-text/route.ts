567be5c8b804a14535df0c9d4816dd03744c5b86
jasangill1 <56135840+jasangill1@users.noreply.github.com>
2023-04-27 01:03:38 -0700

    somewhere

--- app/api/generate-text/route.ts
+++ app/api/generate-text/route.ts
@@ -1,29 +1,25 @@
 // app/api/generate-text/route.ts
 import { NextApiRequest, NextApiResponse } from 'next';
-import openai from 'openai';
-
-openai.apiKey = process.env.OPENAI_API_KEY;
-
+import openai from '@/lib/opeaiInstance';
 
 export async function post(req: NextApiRequest, res: NextApiResponse): Promise<void> {
-    const requestBody = req.body;
-    const prompt = requestBody.prompt;
-  
-    try {
-      const result = await openai.ChatCompletion.create({
-        model: 'gpt-3.5-turbo',
-        messages: [
-          { role: 'system', content: 'You are a helpful assistant.' },
-          { role: 'user', content: prompt },
-        ],
-      });
-  
-      const generatedText = result.choices[0].message.content.trim();
-  
-      res.status(200).json({ generatedText });
-    } catch (error) {
-      console.error('Error calling OpenAI API:', error);
-      res.status(500).json({ error: 'Error calling OpenAI API' });
-    }
+  const requestBody = req.body;
+  const prompt = requestBody.prompt;
+
+  try {
+    const result = await openai.ChatCompletion.create({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        { role: 'system', content: 'You are a helpful assistant.' },
+        { role: 'user', content: prompt },
+      ],
+    });
+
+    const generatedText = result.choices[0].message.content.trim();
+
+    res.status(200).json({ generatedText });
+  } catch (error) {
+    console.error('Error calling OpenAI API:', error);
+    res.status(500).json({ error: 'Error calling OpenAI API' });
   }
-  
+}
